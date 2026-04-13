7eb7f8c5b73833448c25ce4917950979a2d013e2
Jonnywik <161025061+Jonnywik@users.noreply.github.com>
2026-04-13 14:10:10 +0800

    Update movement.py

--- battle_tanks/components/movement.py
+++ battle_tanks/components/movement.py
@@ -1,3 +1,4 @@
+#imports
 import pygame as pg
 from typing import Union
 from battle_tanks.components.network import NetworkComponent
@@ -13,37 +14,29 @@ class MovementComponent:
         self.network = network
         self.player = player
 
-
+""" Translates input into movement """
     def keys(self):
         key = pg.key.get_pressed()
         actions = []
-
         if key[pg.K_d]:
-            action = Struct.RIGHT_EVENT_PLAYER
-            actions.append(action)
-
-        elif key[pg.K_a]:
             action = Struct.LEFT_EVENT_PLAYER
             actions.append(action)
-
+        elif key[pg.K_a]:
+            action = Struct.RIGHT_EVENT_PLAYER
+            actions.append(action)
         elif key[pg.K_w]:
             action = Struct.UP_EVENT_PLAYER
             actions.append(action)
-
         elif key[pg.K_s]:
             action = Struct.DOWN_EVENT_PLAYER
             actions.append(action)
-
-        if key[pg.K_i]:
+        if key[pg.K_p]:
             action = Struct.LEFT_ANGLE_EVENT_PLAYER
             actions.append(action)
-
-        elif key[pg.K_p]:
+        elif key[pg.K_i]:
             action = Struct.RIGHT_ANGLE_EVENT_PLAYER
             actions.append(action)
 
+#network transmission(queues action pressed to be processed on a server if internet connection is present)
         if self.network and len(actions) > 0:
             self.network.send_keys(actions)
-
-
-
